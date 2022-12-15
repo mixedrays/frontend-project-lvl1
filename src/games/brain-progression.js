a18c4922fe0c91fbe5a575720e306caf4c665cd8
Aleks M <aleksm@Alekss-MacBook-Pro.local>
2022-12-15 22:09:06 +0100

    Update brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,15 +4,15 @@ import { askForUserAnswer, showQuestion } from '../cli.js';
 export const gameRules = 'What number is missing in the progression?';
 
 export const gameRound = () => {
-  const progressionLength = getRandomNumber(5, 10);
+  const progressionLength = getRandomNumber(6, 11);
   const progressionStep = getRandomNumber(1, 15);
   const progressionFirst = getRandomNumber(0, 50);
-  const hiddenPosition = getRandomNumber(0, progressionLength);
+  const hiddenPosition = getRandomNumber(0, progressionLength - 1);
   const progression = Array(progressionLength).fill(0)
     .map((_, index) => progressionFirst + index * progressionStep);
   const hiddenNumber = progression[hiddenPosition];
   progression[hiddenPosition] = '..';
-  const question = progression.reduce((acc, element) => `${acc} ${element}`, '');
+  const question = progression.join(' ');
 
   showQuestion(question);
 
